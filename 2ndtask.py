dd23902a882f9641f7a196d5fb6367d137b66caf
Priyajangid03 <jangidpriya699@gmail.com>
2025-06-29 21:23:37 +0530

    Add files via upload

---
+++ 2ndtask.py
@@ -0,0 +1,4 @@
+
+number_ofcameras_sold = int(input("Enter the the number of total cameras sold:"))
+total_priceof_cameras = int(input("Enter the total price of cameras sold:"))
+print("total salary is:",1500 + number_ofcameras_sold*200 + total_priceof_cameras*0.02)
