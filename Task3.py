a3c081d7269c00f5735cd244dc13948841f27da2
Priyajangid03 <jangidpriya699@gmail.com>
2025-06-30 21:30:45 +0530

    Add files via upload
    Checking Divisibility

---
+++ Task3.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the number:"))
+
+if a%3 == 0 and a%5 == 0:     
+     print("Number is divisible by both 3 and 5")       
+
+elif a%5 == 0:
+    print("Number is divisible by 5")
+
+elif a%3 == 0:
+     print("Number is divisible by 3")
+
+else:
+    print("Number is not divisible")
+   
+
+
