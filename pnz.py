eee9803ff52d9cc77ef81a02aa0912f0a158846b
Priyajangid03 <jangidpriya699@gmail.com>
2025-07-03 21:52:46 +0530

    Add files via upload
    Positive, Negative or Zero

---
+++ pnz.py
@@ -0,0 +1,11 @@
+#Checking whether the given input number is positive, negative or zero
+input = int(input("Enter the Number: "))
+
+if input > 0:
+    print("The number is positive")
+
+elif input < 0:
+    print("The number is negative")
+
+else:
+    print("The number is zero")
