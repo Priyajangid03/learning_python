4d9adbf4501083630a7c3d4a3266e543a135d2f0
Priyajangid03 <jangidpriya699@gmail.com>
2025-07-03 21:51:58 +0530

    Add files via upload
    Even or Odd Number

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+#Checking whether the given input is odd or even number
+input = int(input("Enter the number: "))
+
+if input % 2 == 0:
+    print("This is an Even Number")
+else:
+    print("This is an Odd Number")
